ca81543dc85e4fd58ec69b2e7da74f36a3fcdb86
Noah C <ncukiert@gmail.com>
2019-01-28 19:54:12 -0800

    Added Bootstrap, jQuery, Default Setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Home Page:
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+// 20 Questions:
+router.get('/20-questions', function(req, res, next) {
+  res.render('20questions');
+});
+
+// Hangman:
+router.get('/hangman', function(req, res, next) {
+  res.render('hangman');
 });
 
 module.exports = router;
